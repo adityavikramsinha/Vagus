7588b5dd6c4b6472e34f2e2d8338736b0c84890c
avs <adityavikramsinha19@gmail.com>
2025-04-14 01:40:46 +0530

    buttons were changed to something a bit more streamlined

--- src/visualise-graphs/components/action-buttons/PrevButton.tsx
+++ src/visualise-graphs/components/action-buttons/PrevButton.tsx
@@ -5,9 +5,9 @@ import Button from "./Button";
 
 const PrevButtonIcon= (props: React.SVGProps<SVGSVGElement>)=> {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none" >
-            <path d="M4.80859 11.1969C4.80859 11.1969 8.54798 14.7053 11.4784 11.9364C13.182 10.3268 13.2248 6.96281 11.4784 5.41602C8.85645 3.09371 4.80859 6.74129 4.80859 6.74129" stroke="#FE640B" strokeWidth="2.5"/>
-            <path d="M3.21695 8.12877L3.89018 5.1894C3.98172 4.78973 4.40875 4.65587 4.65885 4.94845L6.49814 7.10021C6.74823 7.39279 6.6338 7.89238 6.29217 7.99947L3.77966 8.78707C3.43803 8.89417 3.12541 8.52844 3.21695 8.12877Z" fill="#FE640B" stroke="#FE640B"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
+            <path d="M5.66683 2.8335L2.8335 5.66683L5.66683 8.50016" stroke="#FE640B" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M2.8335 5.66699H10.271C12.4226 5.66699 14.1668 7.41119 14.1668 9.56283C14.1668 11.7145 12.4226 13.4587 10.271 13.4587H3.54183" stroke="#FE640B" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
     )
 }
--- src/visualise-graphs/components/action-buttons/StartButton.tsx
+++ src/visualise-graphs/components/action-buttons/StartButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Node from "../../ts/Node";
 import currentState from "../../ts/GlobalState";
 import {removeAllClasses} from "../../ts/Utility";
-import {NOTSET} from "../../ts/Types";
+import {AlgoType, NOTSET} from "../../ts/Types";
 import Button from "./Button";
 import useStateManager, {NodeType} from "../../store/FrontendStateManager";
 import Syncer from "../../store/Syncer";
@@ -10,11 +10,15 @@ import Syncer from "../../store/Syncer";
 
 const StartButtonIcon = (props: React.SVGProps<SVGSVGElement>) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none" id="run-button"
-             className="button" {...props}>
-            <path
-                d="M13.75 7.20096C14.75 7.77831 14.75 9.22169 13.75 9.79904L7 13.6962C6 14.2735 4.75 13.5518 4.75 12.3971L4.75 4.60288C4.75 3.44818 6 2.7265 7 3.30385L13.75 7.20096Z"
-                fill="#00FF2F"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
+            <g clip-path="url(#clip0_80_24)">
+                <path d="M14.5 7.63398C15.1667 8.01888 15.1667 8.98112 14.5 9.36602L6.25 14.1292C5.58333 14.5141 4.75 14.0329 4.75 13.2631L4.75 3.73686C4.75 2.96706 5.58333 2.48593 6.25 2.87083L14.5 7.63398Z" stroke="#00FF2F" fill="#00FF2F" fillOpacity="0.1"/>
+            </g>
+            <defs>
+                <clipPath id="clip0_80_24">
+                    <rect width="17" height="17" fill="white"/>
+                </clipPath>
+            </defs>
         </svg>
     )
 }
@@ -44,6 +48,7 @@ const startButtonClick = (
                 srcNode.getAdjNodes().forEach(edge => Syncer.updateEdge(edge.dest.getData(), id));
             }
         }
+        console.log(useStateManager.getState().activeFiles.ts, AlgoType);
     }
 }
 
