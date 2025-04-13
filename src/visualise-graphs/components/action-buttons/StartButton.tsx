0832ee70eee6b4479878ba7be0b563359c4a175b
avs <adityavikramsinha19@gmail.com>
2025-04-13 23:06:53 +0530

    cleaning up, since the mess was becoming too much to navigate. It is easier to modularly clean up and fix than to keep both the prev+curr

--- src/visualise-graphs/components/action-buttons/StartButton.tsx
+++ src/visualise-graphs/components/action-buttons/StartButton.tsx
@@ -2,13 +2,7 @@ import React from "react";
 import Node from "../../ts/Node";
 import currentState from "../../ts/GlobalState";
 import {removeAllClasses} from "../../ts/Utility";
-import {AlgoType, NOTSET, NOTSET_TYPE} from "../../ts/Types";
-import Algorithms from "../../ts/Algorithms";
-import {
-    updateBiDirectionalVisitedNodes,
-    updateRandomVisitedNodes,
-    updateVisitedNodes
-} from "../../ts/HexBoardAlgoRunUpdate";
+import {NOTSET} from "../../ts/Types";
 import Button from "./Button";
 import useStateManager, {NodeType} from "../../store/FrontendStateManager";
 import Syncer from "../../store/Syncer";
@@ -30,7 +24,6 @@ const startButtonClick = (
     currentNode: Node<number>,
 ): void => {
     if (!useStateManager.getState().block) {
-        let remAlgo: AlgoType[] = [AlgoType.A_STAR_SEARCH, AlgoType.BELLMAN_FORD, AlgoType.BEST_FIRST_SEARCH, AlgoType.BREADTH_FIRST_SEARCH, AlgoType.DEPTH_FIRST_SEARCH, AlgoType.DEPTH_FIRST_SEARCH, AlgoType.DIJKSTRAS_SEARCH]
         if (useStateManager.getState().activeFiles.ts === NOTSET) {
             alert('Please select an algorithm before continuing!');
             return;
@@ -51,58 +44,6 @@ const startButtonClick = (
                 srcNode.getAdjNodes().forEach(edge => Syncer.updateEdge(edge.dest.getData(), id));
             }
         }
-        if (useStateManager.getState().activeFiles.ts === AlgoType.BI_DIRECTIONAL_SEARCH) {
-            removeAllClasses(1, []);
-            const [pathFromStart, visitedFromStartSet, visitedFromEndSet] = new Algorithms(
-                currentState.graph()).biDirectional(useStateManager.getState().startNodeId,
-                                                    useStateManager.getState().endNodeId);
-            const visitedFromStartArray = Array.from(visitedFromStartSet);
-            const visitedFromEndArray = Array.from(visitedFromEndSet);
-            if (useStateManager.getState().bombNodeId !== NOTSET)
-                alert("BiDirectional Search cannot be used with a bomb node!");
-            else if (visitedFromStartArray.length > visitedFromEndArray.length) {
-                updateBiDirectionalVisitedNodes(visitedFromStartArray, pathFromStart, true, 0);
-                updateBiDirectionalVisitedNodes(visitedFromEndArray, pathFromStart, false, 0);
-            } else {
-                updateBiDirectionalVisitedNodes(visitedFromStartArray, pathFromStart, false, 0);
-                updateBiDirectionalVisitedNodes(visitedFromEndArray, pathFromStart, true, 0);
-            }
-        } else if (useStateManager.getState().activeFiles.ts === AlgoType.RANDOM_WALK && currentState.run()) {
-            let endNode: number | NOTSET_TYPE = useStateManager.getState().endNodeId;
-            updateRandomVisitedNodes(currentNode.getData())
-            let oldNode = currentNode;
-            currentNode = currentNode.getRandomNeighbour()
-            if (currentState.bombNode() != null)
-                alert("Random Walk cannot be used with a bomb node!");
-            else if (currentNode === oldNode)
-                alert("No Path Found! :(");
-            else if (currentNode.getData() !== endNode)
-                startButtonClick(currentNode);
-            else if (currentNode.getData() === endNode)
-                updateRandomVisitedNodes(endNode);
-        } else if (remAlgo.includes(useStateManager.getState().activeFiles.ts)) {
-            removeAllClasses(1, []);
-            if (useStateManager.getState().bombNodeId === NOTSET) {
-                let path: number[] = Algorithms.runAlgoFromGlobalStateNoBomb().path;
-                let visitedInOrder: Set<number> = Algorithms.runAlgoFromGlobalStateNoBomb().visitedInOrder;
-                let ids: number[] = Array.from(visitedInOrder.keys());
-                updateVisitedNodes(ids, null, path, false, 0);
-            } else {
-                let path: number[] = Algorithms.runAlgorithmGlobalStateYesBomb().path;
-                let visitedP1: Set<number> = Algorithms.runAlgorithmGlobalStateYesBomb().visitedP1;
-                let visitedP2: Set<number> = Algorithms.runAlgorithmGlobalStateYesBomb().visitedP2;
-                let ids1: number[] = Array.from(visitedP1.keys());
-                let ids2: number[] = Array.from(visitedP2.keys());
-                updateVisitedNodes(ids1, ids2, path, true, 0);
-            }
-        }
-    } else {
-        removeAllClasses(1, []);
-        currentState.changeRun();
-        setTimeout(() => {
-            currentState.changeRun();
-            startButtonClick(currentNode);
-        }, 250)
     }
 }
 
