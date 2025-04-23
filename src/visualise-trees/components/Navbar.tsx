77909e5a4d853e8f782c80d4f40cd1c0372dd51b
avs <adityavikramsinha19@gmail.com>
2025-04-23 21:38:10 +0530

    /trees route updated to reflect changes

--- src/visualise-trees/components/Navbar.tsx
+++ src/visualise-trees/components/Navbar.tsx
@@ -2,183 +2,29 @@ import * as React from "react";
 import {FC} from "react";
 
 
-
-import {
-    BombNodeIcon,
-    EndNodeIcon,
-    IOFileIcon,
-    ShortestPathNodeIcon,
-    StartNodeIcon,
-    TsFileIcon,
-    UnvisitedNodeIcon,
-    VisitedNodeIcon,
-    WallNodeIcon,
-    WeightNodeIcon
-} from "../../components/file/FileIcons";
+import {TsFileIcon} from "../../components/file/FileIcons";
 import Folder from "../../components/folder/Folder";
-import {FileType} from "../../components/file/File";
-import File from "../../components/file/File";
+import File, {FileType} from "../../components/file/File";
 
 const Navbar: FC = () => {
     return (
-        <div className="navbar">
-            <div className="header">
-                <div className="title">Vagus</div>
+        <div className="w-[30vw] border-r border-r-cmd-border bg-cmd-bg h-full text-white select-none">
+            <div className="h-1/10 font-medium flex items-center justify-between pr-4 pl-4">
+                <div className="text-2xl">Vagus</div>
             </div>
-            <div className="folder-panel">
+            <div className="
+                            pt-[5px]
+                            h-[86%]
+                            overflow-y-auto
+                            [scrollbar-color:var(--color-moz-sb-color)_transparent]
+                            [&::-webkit-scrollbar]:w-[5px]
+                            [&::-webkit-scrollbar-thumb]:bg-[var(--color-sb-color)]
+                            [&::-webkit-scrollbar-thumb:hover]:bg-[var(--color-sb-color-hover)]
+                            [&::-webkit-scrollbar-track]:bg-[var(--color-sb-color-track)]
+                            ">
                 <Folder name="graphs">
-
                     <Folder name="algorithms">
-
-                        <Folder name="heuristic">
-                            <File
-                                name="aStarSearch.ts"
-                                id="ts-1"
-                                type={FileType.TS}
-                                Icon={<TsFileIcon/>}
-                            />
-                            <File
-                                name="bestFirstSearch.ts"
-                                id="ts-2"
-                                type={FileType.TS}
-                                Icon={<TsFileIcon/>}
-                            />
-                        </Folder>
-                        <Folder name="un-weighted">
-                            <File
-                                name="breadthFirstSearch.ts"
-                                id="ts-3"
-                                type={FileType.TS}
-                                Icon={<TsFileIcon/>}
-                            />
-                            <File
-                                name="depthFirstSearch.ts"
-                                id="ts-4"
-                                type={FileType.TS}
-                                Icon={<TsFileIcon/>}
-                            />
-                            <File
-                                name="randomWalk.ts"
-                                id="ts-5"
-                                type={FileType.TS}
-                                Icon={<TsFileIcon/>}
-                            />
-                            <File
-                                name="bestFirstSearch.ts"
-                                id="ts-6"
-                                type={FileType.TS}
-                                Icon={<TsFileIcon/>}
-                            />
-                        </Folder>
-                        <Folder name="weighted">
-                            <File
-                                name="aStarSearch.ts"
-                                id="ts-7"
-                                type={FileType.TS}
-                                Icon={<TsFileIcon/>}
-                            />
-                            <File
-                                name="dijkstrasSearch.ts"
-                                id="ts-8"
-                                type={FileType.TS}
-                                Icon={<TsFileIcon/>}
-                            />
-                            <File
-                                name="bellmanFord.ts"
-                                id="ts-9"
-                                type={FileType.TS}
-                                Icon={<TsFileIcon/>}
-                            />
-                            <File
-                                name="biDirectionalSearch.ts"
-                                id="ts-10"
-                                type={FileType.TS}
-                                Icon={<TsFileIcon/>}
-                            />
-                        </Folder>
-
-                    </Folder>
-                    <Folder name="addableNodes">
-                        <File
-                            name="startNode.io"
-                            id="io-1"
-                            type={FileType.IO}
-                            Icon={<IOFileIcon/>}
-                        />
-                        <File
-                            name="endNode.io"
-                            id="io-2"
-                            type={FileType.IO}
-                            Icon={<IOFileIcon/>}
-                        />
-                        <File
-                            name="bombNode.io"
-                            id="io-3"
-                            type={FileType.IO}
-                            Icon={<IOFileIcon/>}
-                        />
-                        <File
-                            name="weightNode.io"
-                            id="io-4"
-                            type={FileType.IO}
-                            Icon={<IOFileIcon/>}
-                        />
-                        <File
-                            name="wallNode.io"
-                            id="io-5"
-                            type={FileType.IO}
-                            Icon={<IOFileIcon/>}
-                        />
-                    </Folder>
-                    <Folder name="legend">
-                        <File
-                            name="bombNode.gui"
-                            id="gui-1"
-                            Icon={<BombNodeIcon/>}
-                            type={FileType.GUI}
-                        />
-                        <File
-                            name="shortestPathNode.gui"
-                            id="gui-2"
-                            Icon={<ShortestPathNodeIcon/>}
-                            type={FileType.GUI}
-                        />
-                        <File
-                            name="wallNode.gui"
-                            id="gui-3"
-                            Icon={<WallNodeIcon/>}
-                            type={FileType.GUI}
-                        />
-                        <File
-                            name="visitedNode.gui"
-                            id="gui-4"
-                            Icon={<VisitedNodeIcon/>}
-                            type={FileType.GUI}
-                        />
-                        <File
-                            name="unvisitedNode.gui"
-                            id="gui-5"
-                            Icon={<UnvisitedNodeIcon/>}
-                            type={FileType.GUI}
-                        />
-                        <File
-                            name="startNode.gui"
-                            id="gui-6"
-                            Icon={<StartNodeIcon/>}
-                            type={FileType.GUI}
-                        />
-                        <File
-                            name="endNode.gui"
-                            id="gui-7"
-                            Icon={<EndNodeIcon/>}
-                            type={FileType.GUI}
-                        />
-                        <File
-                            name="weightNode.gui"
-                            id="gui-8"
-                            Icon={<WeightNodeIcon/>}
-                            type={FileType.GUI}
-                        />
+                        <File type={FileType.TS} id={'ts-1'} name="minimumSpanningTree" Icon={<TsFileIcon/>}/>
                     </Folder>
                 </Folder>
             </div>
--- src/visualise-trees/components/TreeVisualiser.tsx
+++ src/visualise-trees/components/TreeVisualiser.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import HexBoard from "@graph/components/hex-board/HexBoard";
 import Navbar from "./Navbar"
 import {StoreProvider} from "../../providers/StoreProvider";
 import {treeStore} from "../../stores/TreeStore";
@@ -9,12 +8,8 @@ export const TreeVisualiserApp: React.FC = () => {
     return (
         <React.Fragment>
             <StoreProvider useStore={treeStore}>
-                <div className="App">
-                    <div className="content">
-                        <div className="left-cmd" id="left-cmd">
-                            <Navbar/>
-                        </div>
-                    </div>
+                <div className="w-full h-screen flex bg-black">
+                    <Navbar/>
                 </div>
             </StoreProvider>
         </React.Fragment>
