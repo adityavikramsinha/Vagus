131267e0e010f09cf994c9cbd7f4a27a6ccb6893
avs <adityavikramsinha19@gmail.com>
2025-04-25 20:06:17 +0530

    fixed, hex hover effects issue.

--- src/visualise-graphs/components/hex/Hex.tsx
+++ src/visualise-graphs/components/hex/Hex.tsx
@@ -64,19 +64,29 @@ const Hex: React.FC<HexProps> = ({x, y, id}) => {
         top: `${y}px`,
     };
     return (
-        <div className={`absolute flex items-center h-[34px] w-[34px] justify-center overflow-hidden`} style={styles}
-             onClick={handleHexClick}>
+        <div onClick={handleHexClick}
+             className={`absolute flex items-center h-[34px] w-[34px] justify-center overflow-hidden group`} style={styles}>
             <HexIcon className={cn(
-                "fill-hex-color", {
+                `fill-hex-color
+                transition-colors
+                duration-[750ms]
+                ease-out
+                `, {
                     "fill-visited-node-color-1 transition-all duration-[1s] animate-visited-node-without-bomb": (visited === NodeType.START_NODE) && !pathNode,
                     "fill-visited-node-color-11 transition-all duration-[1s] animate-visited-node-with-bomb": (visited === NodeType.BOMB_NODE) && !pathNode,
                     "fill-path-node-color-1 transition-all duration-[750ms] animate-path": pathNode,
                     "fill-wall-node-color": isWallNode,
                     "animate-random-walk": isRandomAlgorithmSteppingStone,
-                })}/>
+                    "group-hover:fill-emerald-400 group-hover:animate-pulse" :
+                        !isWallNode &&
+                        !pathNode &&
+                        !isRandomAlgorithmSteppingStone &&
+                        !(visited===NodeType.START_NODE || visited === NodeType.BOMB_NODE)
+                })}
+            />
             <div className={cn(
                 "absolute z-[1] w-full h-full justify-center items-center")}>
-                {isStartNode && <NodeIcon.StartNodeIcon/>}
+                {isStartNode && <NodeIcon.StartNodeIcon />}
                 {isBombNode && <NodeIcon.BombNodeIcon/>}
                 {isEndNode && <NodeIcon.EndNodeIcon/>}
                 {isWeightNode && <NodeIcon.WeightNodeIcon/>}
