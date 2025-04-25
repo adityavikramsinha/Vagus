691212c44edfaee01fc6b7af251e0d0c20ea499e
avs <adityavikramsinha19@gmail.com>
2025-04-25 18:50:43 +0530

    use turbopack mate

--- src/visualise-graphs/api/Pipe.ts
+++ src/visualise-graphs/api/Pipe.ts
@@ -34,9 +34,12 @@ export default class Pipe {
     }
 
     /**
-     * Implementation of Szudzik's Elegant Pair function for two integers. It pipes 2 numbers through a tunnel
-     * and returns a collision free deterministic and unique consolidated integer every single time, and can serve
-     * the purpose of UUID in this case since it is easily reproducible. See {@link [this](http://szudzik.com/ElegantPairing.pdf)} for detailed paper
+     * Implementation of Szudzik's Elegant Pair function for two integers.
+     * It pipes 2 numbers through a tunnel
+     * and returns a collision free deterministic and unique consolidated integer
+     * every single time, and can serve
+     * the purpose of UUID in this case since it is easily reproducible.
+     * See {@link [this](http://szudzik.com/ElegantPairing.pdf)} for detailed paper
      * and {@link [here](https://stackoverflow.com/questions/919612/mapping-two-integers-to-one-in-a-unique-and-deterministic-way)} for reference
      * @param x
      * @param y
