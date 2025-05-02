5faa15e7e9158dd31c0deec6f2c4bb550b09ee11
avs <adityavikramsinha19@gmail.com>
2025-05-03 00:40:55 +0530

    animator.ts was made less janky

--- src/visualise-graphs/api/Animator.ts
+++ src/visualise-graphs/api/Animator.ts
@@ -13,7 +13,7 @@ export default class Animator {
             await Syncer.supervise(async () => {
                 internalSet.add(node);
                 useGraphStore.setState({pathNodes: new Set(internalSet)});
-                await new Promise(res => setTimeout(res, 10)); // Delay for smooth animation
+                await new Promise(res => setTimeout(res, 2)); // Delay for smooth animation
             })
         }
         // for the last one.
@@ -23,16 +23,11 @@ export default class Animator {
     static async animateVisitedNodes(visited: Map<string, NodeType.START_NODE | NodeType.BOMB_NODE>) {
         const store = useGraphStore.getState();
         const internalSet = store.visitedNodes;
-        let i = 0;
         for (const entry of visited) {
             await Syncer.supervise(async () => {
                 internalSet.set(entry[0], entry[1]);
-                i++;
-                // Every 4 nodes, just debounce.
-                if (i % 4 === 0) {
-                    useGraphStore.setState({visitedNodes: new Map(internalSet)});
-                    await new Promise(res => setTimeout(res, 2)); // Delay for smooth animation
-                }
+                useGraphStore.setState({visitedNodes: new Map(internalSet)});
+                await new Promise(res => setTimeout(res, 2)); // Delay for smooth animation
             })
 
         }
@@ -44,7 +39,7 @@ export default class Animator {
 
     // return if there is no neighbour
     // return if there is a start node has come
-    static async animateRandomWalk(nodeId: string| NOTSET_t): Promise<void> {
+    static async animateRandomWalk(nodeId: string | NOTSET_t): Promise<void> {
         if (useGraphStore.getState().endNodeId === nodeId ||
             BackendStateManager.graph().vertices().get((nodeId as string)).getAdjVertices().size === 1) {
             if (useGraphStore.getState().endNodeId === nodeId) {
@@ -53,7 +48,7 @@ export default class Animator {
             }
             return;
         }
-        await Syncer.supervise( async () => {
+        await Syncer.supervise(async () => {
             useGraphStore.setState({randomPathId: nodeId});
             useGraphStore.getState().pathNodes.add(nodeId);
             await new Promise(res => setTimeout(res, 250));
