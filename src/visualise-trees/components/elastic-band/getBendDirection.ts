f90084a36ba2adba9a56155c7b9e7901f4ab7589
avs <adityavikramsinha19@gmail.com>
2025-05-02 11:05:40 +0530

    some minor updates

--- src/visualise-trees/components/elastic-band/getBendDirection.ts
+++ src/visualise-trees/components/elastic-band/getBendDirection.ts
@@ -2,10 +2,8 @@ import BackendStateManager from "../../api/BackendStateManager";
 
 const getBendDirection = (srcId: string, destId: string): 1 | -1 | 0 => {
     const graph = BackendStateManager.graph;
-    const srcHasDest = graph.vertices().get(srcId)?.getAdjVertices()
-                            .some(edge => edge.dest.getData() === destId);
-    const destHasSrc = graph.vertices().get(destId)?.getAdjVertices()
-                            .some(edge => edge.dest.getData() === srcId);
+    const srcHasDest = graph.vertices().get(srcId)?.getAdjVertices().has(destId)
+    const destHasSrc = graph.vertices().get(destId)?.getAdjVertices().has(srcId)
 
     if (srcHasDest && destHasSrc) {
         // Arbitrary tie-breaker to decide curve direction
